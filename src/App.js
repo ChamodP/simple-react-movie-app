a5faee330606169041b121de0a6522e4eee32c2d
ChamodP <chamodp.jr@gmail.com>
2024-01-09 14:46:43 +0530

    names of movies are showing after hitting enter

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [input, setInput] = useState('')
-  const url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+  const [movies, setMovies] = useState([]);
 
-  const searchMovie =  async (title) =>{
-    const response = await fetch(`${url}&s=${title}`)
-    const data = await response.json()
+  const url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
+
+  const searchMovie = async (title) => {
+    const response = await fetch(`${url}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search || [])
     console.log(data.Search);
-  }
+  };
 
   const displayMovieNames = () => {
-    return 
-  }
+    return movies.map((movie)=>(<li key={movie.imdbID}>{movie.Title}</li>))
+  };
 
-  useEffect(()=>{
-    searchMovie(input)
-  }, [])
 
   return (
     <div className="App">
       <h1>MovieSearch Hub</h1>
-      <input type="text" value={input} onChange={(event) => {
-        setInput(event.target.value)
-      }}/>
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+      />
       <button onClick={() => searchMovie(input)}>Search</button>
+      <ul>
+        {displayMovieNames()}
+      </ul>
     </div>
   );
 }
